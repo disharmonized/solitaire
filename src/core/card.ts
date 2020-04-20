65e2fcb27ab1f35d7196a3bad485f69973bb1c3c
disharmonized <critbrain@gmail.com>
2020-04-21 00:12:31 +0300

    Refactor card

--- src/core/card.ts
+++ src/core/card.ts
@@ -1,31 +1,22 @@
-import { Suit } from 'core';
+import { Suit, Rank } from 'core';
 
 /**
  * Card class
  */
 export class Card {
-  private _isFacedUp: boolean;
-  private _rank: number;
-  private _suit: Suit;
-
   /**
    * Card constructor
    * @param {*} suit
    * @param {*} rank From 1 to 13 (1 - Ace, 11 - Jack, 12 - Quenn, 13 - King)
    * @param {*} isFacedUp Whether card is faced up or not
    */
-  constructor(suit: Suit, rank: number, isFacedUp = false) {
-    this._suit = suit;
-    //todo validate
-    this._rank = rank;
-    this._isFacedUp = isFacedUp;
-  }
+  constructor(private _suit: Suit, private _rank: Rank, private _isFacedUp = false) {}
 
   get isFacedUp(): boolean {
     return this._isFacedUp;
   }
 
-  get rank(): number {
+  get rank(): Rank {
     return this._rank;
   }
 
--- src/core/index.ts
+++ src/core/index.ts
@@ -17,8 +17,8 @@ export enum SpecialRank {
   QUENN = 12,
   KING = 13,
 }
-export type SimpleRank = 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
-export type Rank = SimpleRank | SpecialRank;
+export type NumericRank = 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
+export type Rank = NumericRank | SpecialRank;
 
 export interface MoveCardFromCardStack {
   index: number;
