22f5950041cd87fe0fb4e84cf5a1252e47f39553
disharmonized <critbrain@gmail.com>
2020-04-20 21:55:26 +0300

    Add tests for Util

--- src/core/util.ts
+++ src/core/util.ts
@@ -1,7 +1,12 @@
 /**
- * N -> [0, 1, 2, ..., N-1]
- * @param size
+ * Class Util. Various utility methods.
  */
-export function createArrayOfSeqIndexes(size: number): number[] {
-  return new Array(size).fill(null).map((_v, i) => i);
+export class Util {
+  /**
+   * N -> [0, 1, 2, ..., N-1]
+   * @param size
+   */
+  static createArrayOfSeqIndexes(size: number): number[] {
+    return new Array(size).fill(null).map((_v, i) => i);
+  }
 }
---
+++ test/core/util.test.ts
@@ -0,0 +1,12 @@
+import * as assert from 'assert';
+import { Util } from '../../src/core/util';
+describe('Util', function() {
+  describe('#createArrayOfSeqIndexes()', function() {
+    it('should return correct value for N=1', function() {
+      assert.deepEqual(Util.createArrayOfSeqIndexes(1), [0]);
+    });
+    it('should return correct value for N=3', function() {
+      assert.deepEqual(Util.createArrayOfSeqIndexes(3), [0, 1, 2]);
+    });
+  });
+});
